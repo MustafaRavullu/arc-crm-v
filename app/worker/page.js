0efdd03abe7e67ac927a8a44435678e59c0e53f4
MustafaRavullu <muhammedravullu@gmail.com>
2023-12-19 19:47:30 +0300

    worker menu text

--- app/worker/page.js
+++ app/worker/page.js
@@ -3,10 +3,16 @@ import Link from "next/link";
 export default function WorkerDashboard() {
   return (
     <div className="flex-1 flex justify-center items-center flex-col gap-3">
-      <Link href={"/worker/direct-fiber-sale"} className="simple_button">
+      <Link
+        href={"/worker/direct-fiber-sale"}
+        className="simple_button w-[200px] text-center"
+      >
         İp Satış Transferi
       </Link>
-      <Link href={"/worker/product-operation"} className="simple_button">
+      <Link
+        href={"/worker/product-operation"}
+        className="simple_button w-[200px] text-center"
+      >
         Ürün İşlemi
       </Link>
     </div>
